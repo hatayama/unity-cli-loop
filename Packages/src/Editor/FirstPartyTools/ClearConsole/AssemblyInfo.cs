df98b5882475a250e03de12a88c4606ea50ac183
Masamichi Hatayama <booqle@gmail.com>
2026-05-28 14:02:13 +0900

    chore: Remove unused AssemblyInfo files (#1232)

--- Packages/src/Editor/FirstPartyTools/ClearConsole/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("UnityCLILoop.FirstPartyTools.Editor")]
-[assembly: InternalsVisibleTo("UnityCLILoop.Tests.Editor")]
-[assembly: InternalsVisibleTo("UnityCLILoop.Tests.PlayMode")]
-[assembly: InternalsVisibleTo("UnityCLILoop.Tests.Demo.Editor")]
