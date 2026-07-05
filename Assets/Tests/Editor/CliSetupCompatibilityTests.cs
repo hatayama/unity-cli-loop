48716b6895ac6c70156912500e3bda8c47b018a4
Masamichi Hatayama <booqle@gmail.com>
2026-07-05 17:55:27 +0900

    chore: Architecture guard tests pass again and CLI setup helpers move behind the UI boundary (#1513)

--- Assets/Tests/Editor/CliSetupCompatibilityTests.cs
+++ Assets/Tests/Editor/CliSetupCompatibilityTests.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
 
-using io.github.hatayama.UnityCliLoop.Presentation;
+using io.github.hatayama.UnityCliLoop.Application;
 
 namespace io.github.hatayama.UnityCliLoop.Tests.Editor
 {
