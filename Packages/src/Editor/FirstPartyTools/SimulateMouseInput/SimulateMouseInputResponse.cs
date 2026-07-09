3ebbe0ba8196e203d086265752ea743791cf0f11
Masamichi Hatayama <booqle@gmail.com>
2026-07-09 21:10:26 +0900

    fix: Mouse click and long-press coordinates now match the Game View's actual resolution (#1662)

--- Packages/src/Editor/FirstPartyTools/SimulateMouseInput/SimulateMouseInputResponse.cs
+++ Packages/src/Editor/FirstPartyTools/SimulateMouseInput/SimulateMouseInputResponse.cs
@@ -17,6 +17,15 @@ namespace io.github.hatayama.UnityCliLoop.FirstPartyTools
         public string? Button { get; set; }
         public float? PositionX { get; set; }
         public float? PositionY { get; set; }
+        public string InputCoordinateSystem { get; set; } = "";
+        public string UnityCoordinateSystem { get; set; } = "";
+        public float? GameViewWidth { get; set; }
+        public float? GameViewHeight { get; set; }
+        public float? InputPositionX { get; set; }
+        public float? InputPositionY { get; set; }
+        public float? InjectedUnityPositionX { get; set; }
+        public float? InjectedUnityPositionY { get; set; }
+        public string CoordinateConversionFormula { get; set; } = "";
         public bool InterruptedByPausePoint { get; set; }
         public string? PausePointId { get; set; }
         public int? PausePointHitCount { get; set; }
