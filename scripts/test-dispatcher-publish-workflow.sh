ecbb00473a6403c16825b5217bc7b8c5fe008e47
Masamichi Hatayama <booqle@gmail.com>
2026-08-31 10:07:15 +0900

    feat: graduate v3 to the stable release channel (#2452)

--- scripts/test-dispatcher-publish-workflow.sh
+++ scripts/test-dispatcher-publish-workflow.sh
@@ -97,7 +97,8 @@ test_unprivileged_build_uses_only_the_approved_event_commit() {
   assert_not_contains "INPUT_REF"
   assert_contains '          ref: ${{ github.sha }}'
   assert_count 2 "          persist-credentials: false"
-  assert_contains "github.ref == 'refs/heads/main' || github.ref == 'refs/heads/v3-beta'"
+  assert_contains "if: github.ref == 'refs/heads/main'"
+  assert_not_contains "github.ref == 'refs/heads/main' || github.ref == 'refs/heads/v3-beta'"
 }
 
 test_publish_validates_metadata_without_checking_out_source() {
